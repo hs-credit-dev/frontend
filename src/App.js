f25f5c67da1fe8bd611684d3768f2bb592112556
Leonidas <ldbrooks297@gmail.com>
2021-06-17 18:58:11 -0700

    create basic autheticated route structure
    -private routes
    -react router dom "Switch"
    -jwt storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
-import logo from './logo.svg';
+//imports 
+
+import React, { Component, useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utilities/setAuthToken';
+
+//styling
+
 import './App.css';
 
+//components
+
+// import Navbar from './components/Navbar';
+
+//private routes for authorized users
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    let token = localStorage.getItem('jwtToken');
+    console.log('>>>>inside private route');
+    return <Route {...rest} render={(props) => {
+        return token ? <Component {...rest} /> : <Redirect to="/login" />
+    }} />
+}
+
+//main parent component to render application
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const [currentUser, setCurrentUser] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+    useEffect(() => {
+        let token;
+        //initializing token
+
+        if(!localStorage.getItem('jwtToken')) {
+            setIsAuthenticated(false);
+            console.log('>>> unauthorized user, no token');
+        } else {
+            token = jwt_decode(localStorage.getItem('jwtToken'));
+            setAuthToken(localStorage.getItem('jwtToken'));
+            setCurrentUser(token);
+        }
+    }, []);
+
+    const nowCurrentUser = (userData) => {
+        console.log('>>> nowCurrentUser');
+        setCurrentUser(userData);
+        setIsAuthenticated(true);
+        console.log('>>> user: userData, auth: true')
+    }
+
+    const handleLogout = () => {
+        console.log('>>> handleLogout');
+        if(localStorage.getItem('jwtToken')) {
+            localStorage.removeItem('jwtToken');
+            setCurrentUser(null);
+            setIsAuthenticated(false);
+            console.log('>>> user: null, auth: false');
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="appNameHeader">
+                hs.Credit
+            </div>
+            {/* 
+            <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
+
+            <div className="mainAppContainer">
+                <Switch>
+                    <Route exact path='signup' component={Signup} />
+                    <Route exact path='/login' render={(props) => 
+                    <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser} />} />
+                    <PrivateRoute exact path='/profile' component={Profile} user={currentUser} handleLogout={handleLogout} />
+                    <Route exact path='/' component={Welcome} />
+                </Switch>
+            </div>
+            <Footer /> 
+            */}
+        </div>
+    );
 }
 
 export default App;
