6ed20e896f4b1c8e1ba8146947c5f40c3043882a
Leonidas <ldbrooks297@gmail.com>
2021-06-17 20:40:49 -0700

    basic routes for testing

---
+++ backend/routes/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    user: require('./user')
+    //will require more exports in future for route handling
+}
---
+++ backend/routes/user.js
@@ -0,0 +1,23 @@
+//imports
+
+const router = require('express').Router()
+const ctrl = require('./controllers')
+const passport = require('passport')
+
+//routes
+
+router.get('/test', ctrl.user.test)
+
+router.post('/register', ctrl.user.register)
+
+router.post('/login', ctrl.user.login)
+
+router.get('/profile', passport.authenticate('jwt', { session: false }), ctrl.user.profile)
+
+router.get('/info', ctrl.user.info)
+
+router.put('/update', ctrl.user.update)
+
+router.get('/:id', ctrl.user.user)
+
+module.exports = router
