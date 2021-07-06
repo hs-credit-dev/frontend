23c61de6c39359c7fade7a1aeccad537bc311307
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-07-06 12:53:42 -0700

    finished educator sign up but need correct api

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,13 @@ import {Switch, Route} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import StudentSignUp from './components/StudentSignUp'
+import EducatorSignUp from './components/EducatorSignUp'
 
 export default(
     <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/studentsignup" component={StudentSignUp}/>
+        <Route exact path="/educatorsignup" component={EducatorSignUp}/>
     </Switch>
 )
