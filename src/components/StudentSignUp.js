cb34b92d10a0bbcb18d2fd26a1214a990cd4dc57
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-08-28 12:35:18 -0700

    currently working on connecting front end student sign up. started form-exploring API call. trying to figure out address + student models

--- src/components/StudentSignUp.js
+++ src/components/StudentSignUp.js
@@ -1,126 +1,3 @@
-// import React from 'react'
-// import { useState } from 'react'
-// import axios from 'axios';
-// import { Redirect } from 'react-router-dom';
-// const { DATABASE_URL } = process.env;
-
-// const StudentSignUp = () => {
-
-    
-//     const [email, setEmail] = useState('');
-//     const [userName, setUserName] = useState('');
-//     const [password, setPassword] = useState('');
-//     const [schoolId, setSchoolId] = useState('');
-//     const [image, setImage] = useState('');
-//     const [confirmPassword, setConfirmPassword] = useState('');
-//     const [redirect, setRedirect] = useState(false);
-
-
-//     const handleGrade = (e) => {
-//         setGrade(e.target.value);
-//     }
-
-//     const handleLocation = (e) => {
-//         setLocation(e.target.value);
-//     }
-
-//     const handleSchool = (e) => {
-//         setSchool(e.target.value);
-//     }
-
-//     const handleImage = (e) => {
-//         setImage(e.target.value);
-//     }
-
-//     const handleUserName = (e) => {
-//         setUserName(e.target.value);
-//     }
-
-//     const handleEmail = (e) => {
-//         setEmail(e.target.value);
-//     }
-    
-
-//     const handlePassword = (e) => {
-//         setPassword(e.target.value);
-//     }
-
-//     const handleConfirmPassword = (e) => {
-//         setConfirmPassword(e.target.value);
-//     }
-//     const handleSchoolId = (e) => {
-//         setSchoolId(e.target.value);
-//     }
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault(); // at the beginning of a submit function
-//         // make sure password and confirm password are equal
-//         // password length >= 8 characters
-//         if (password === confirmPassword && password.length >= 8) {
-//             const userForm = { userName, email, password };
-//            const studentForm = {schoolId, image, about, street1, street2, city, state, zip}
-           
-
-//            const newUser = await axios.post(`${DATABASE_URL}/users/create`, userForm)
-//            const newstudent = await axios.post(`${DATABASE_URL}/students/create`, studentForm)
-
-           
-//             .catch(error => console.log('===> Error in Signup', error));
-//         } else {
-//             if (password !== confirmPassword) return alert('Passwords don\'t match');
-//             alert('Password needs to be at least 8 characters. Please try again.');
-//         }
-//     }
-
-//     if (redirect) return <Redirect to="/login" /> // You can have them redirected to profile (your choice)
-
-//     return (
-//         <div>
-//             <h1 className="student-signup">Student Sign up</h1>
-//             <form className="student-signup-form" onSubmit={handleSubmit}>
-               
-//                     <label>Username</label>
-//                     <input type="text" value={userName} onChange={handleUserName}></input>
-
-//                     <label htmlFor="email">Email</label>
-//                     <input type="email" name="email" value={email} onChange={handleEmail}></input>
-
-//                     <label htmlFor="password">Password</label>
-//                     <input type="password" value={password} onChange={handlePassword}></input>
-              
-//                     <label htmlFor="confirmPassword" >Confirm password</label>
-//                     <input type="password" value={confirmPassword} onChange={handleConfirmPassword}></input>
-
-//                     <label>Street 1</label>
-//                     <input type="text" value={street1} onChange={handleLocation}></input>
-                
-//                     <label>Street 2</label>
-//                     <input type="text" value={street2} onChange={handleLocation}></input>
-                    
-//                     <label>Street 2</label>
-//                     <input type="text" value={street2} onChange={handleLocation}></input>
-
-//                     <label>School</label>
-//                     <input type="text" value={schoolId} onChange={handleSchoolId}></input>
-                
-//                     <label>Grade</label>
-//                     <input type="text" value={grade} onChange={handleGrade}></input>
-               
-//                     <label>Profile Picture</label>
-//                     <input type="select" value={image} onChange={handleImage}></input>
-                
-                    
-                
-                    
-                
-//                 <button value="submit">Sign Up!</button>
-
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default StudentSignUp
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
@@ -128,30 +5,39 @@ import axios from 'axios';
 const REACT_APP_DATABASE_URL = process.env.REACT_APP_DATABASE_URL;
 
 const StudentSignUp = () => {
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [schoolId, setSchoolId] = useState('');
+    const [street1, setStreet1] = useState('');
+    const [street2, setStreet2] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
 
 
 
-    const handleUsername = (e) => {
-        setUsername(e.target.value);
+    const handleSchoolId = (e) => {
+        setSchoolId(e.target.value);
     }
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
+    const handleStreet1 = (e) => {
+        setStreet1(e.target.value);
     }
 
 
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
+    const handleStreet2 = (e) => {
+        setStreet2(e.target.value);
     }
 
-    const handleConfirmPassword = (e) => {
-        setConfirmPassword(e.target.value);
+    const handleCity = (e) => {
+        setCity(e.target.value);
+    }
+    
+    const handleState = (e) => {
+        setState(e.target.value);
     }
 
+    const handleZip = (e) => {
+        setZip(e.target.value);
+    }
 
     const handleSubmit = async (e) => {
         //needs to check for valid email
@@ -163,7 +49,7 @@ const StudentSignUp = () => {
 
             console.log(REACT_APP_DATABASE_URL)
 
-            await axios.post(`${REACT_APP_DATABASE_URL}/users/create`, newUser)
+            await axios.post(`${REACT_APP_DATABASE_URL}/student/create`, newUser)
             // await axios.post(`http://localhost:8000/api/users/create`, newUser)
             
             // .catch(error => console.log('===> Error in Signup', error));
@@ -180,17 +66,23 @@ const StudentSignUp = () => {
             <h1 className="basic-signup">Student Sign up</h1>
             <form className="basic-signup-form" onSubmit={handleSubmit}>
 
-                <label></label>
-                <input type="text" value={username} onChange={handleUsername}></input>
+                <label>School ID</label>
+                <input type="text" value={schoolId} onChange={handleSchoolId}></input>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={email} onChange={handleEmail}></input>
+                <label htmlFor="street 1">Street 1</label>
+                <input type="text" name="street 1" value={street1} onChange={handleStreet1}></input>
 
-                <label htmlFor="password">Password</label>
-                <input type="password" value={password} onChange={handlePassword}></input>
+                <label htmlFor="street 2">Street 2</label>
+                <input type="text" value={street2} onChange={handleStreet2}></input>
 
-                <label htmlFor="confirmPassword" >Confirm password</label>
-                <input type="password" value={confirmPassword} onChange={handleConfirmPassword}></input>
+                <label htmlFor="city" >City</label>
+                <input type="text" value={city} onChange={handleCity}></input>
+
+                <label htmlFor="state">State</label>
+                <input type="text" value={state} onChange={handleState}></input>
+
+                <label htmlFor="zipcode">Zipcode</label>
+                <input type="text" value={zip} onChange={handleZip}></input>
 
                 <button value="submit">Sign Up!</button>
 
