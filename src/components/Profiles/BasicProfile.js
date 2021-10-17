ab9ad58a09eda91e3b3c57cbf2d389b573f638a2
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-10-17 16:56:22 -0700

    lez go

---
+++ src/components/Profiles/BasicProfile.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const BasicProfile = () => {
+
+    
+    return (
+        <div>
+            <img href="./assets/profiledefault" alt="default profile avatar"/>
+            <div>
+            {about}
+<button>edit bio</button>
+<form>
+
+</form>   
+            </div>
+            
+        </div>
+    )
+}
+
+export default BasicProfile
