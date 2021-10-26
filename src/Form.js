37f2c1fee85d2eeb9a77fe85548ea3533e187089
mbrown <tugtuj@gmail.com>
2021-10-25 23:11:12 -0400

    WIP - Setting up Basic User Profile and form, form created with logic. Needs connection to backend

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,24 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 
 const Form = () => {
     const [about, setAbout] = useState('')
 
-    const handleAboutChange = () => {
-
+    const handleAboutChange = (event) => {
+        setAbout(event.target.value)
+        console.log(about)
     }
+
+    const handleSubmit = () => {
+        //post data to backend
+    }
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
           <label>
               Edit About:
               <input type="text" name="about" onChange={handleAboutChange}/>
           </label>
-          <input type="submit" name="submit" />
+          <input type="submit" name="submit"/>
       </form>
     )
 }
--- src/components/Profiles/BasicProfile.js
+++ src/components/Profiles/BasicProfile.js
@@ -1,10 +1,34 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import Form from '../../Form'
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../../utilities/setAuthToken';
 
 const BasicProfile = () => {
+    const [currentUser, setCurrentUser] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(true);
     const [clicked, setClicked] = useState(false)
 
+    useEffect(() => {
+        let token;
+        //initializing token
+
+        if (!localStorage.getItem('jwtToken')) {
+            setIsAuthenticated(false);
+            console.log('>>> unauthorized user, no token');
+        } else {
+            token = jwt_decode(localStorage.getItem('jwtToken'));
+            setAuthToken(localStorage.getItem('jwtToken'));
+            setCurrentUser(token);
+        }
+    }, []);
+
+    const nowCurrentUser = (userData) => {
+        console.log('>>> nowCurrentUser');
+        setCurrentUser(userData);
+        setIsAuthenticated(true);
+        console.log('>>> user: userData, auth: true')
+    }
+    
     const handleClick= () => {
 
         setClicked(!clicked)
@@ -13,12 +37,12 @@ const BasicProfile = () => {
     return (
         <div className="profile-container">
             <img href="./assets/profiledefault" alt="default profile avatar" />
-            {/* <h5>About { isAuthenticated ? user.name : 'User'}</h5> */}
-            {/* <p>{ user.about ? user.about : `${user.name} doesn't have an about section yet.` }</p> */}
+            <h5>About { currentUser.name }</h5>
+            <p>{ currentUser.about ? currentUser.about : `${currentUser.name} doesn't have an about section yet.` }</p>
             <div className="profile-bio-section">
-                {user.about}
+                { currentUser.about }
                 <button onClick={handleClick}>edit bio</button>
-                {clicked ? <Form about={about}/> : ''}
+                {clicked ? <Form /> : ''}
             </div>
 
         </div>
