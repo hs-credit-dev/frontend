53203f343e538f6856ef0604d13f868cb452cc59
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-11-09 14:13:34 -0800

    not able to get bio onto basic user profile cause its not in the ERD. waiting to hear back on this

---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bio = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Bio
---
+++ src/components/EditBioForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditBioForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EditBioForm
