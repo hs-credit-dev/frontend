54f05a770e9c1cf5111d7eaa0a4441b80353b9b8
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-12-13 19:47:17 -0800

    added credit detail page. still in progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <h1>About</h1>
 
-            {/*  who we are, contact, link to other website */}
+
             <p>At hs.credit, we are the gold standard of academic testing. Each month 11th and 12th grade students can mint one credit-bearing NFT, approved by a committee of 3 credit experts. Socio-economically diverse blocks of these NFTs generate academic capital, a cryptocurrency award for participating students. Educators earn up to $180/hr as credit experts on the platform. The result is quality academic data and a steady stream of high quality, hyper-local youth media.</p>
         </div>
     )
---
+++ src/pages/CeditDetails.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactPlayer from "react-player"
+
+const CeditDetails = () => {
+    return (
+        <div>
+            <h1>Credit Details</h1>
+            <ReactPlayer
+                url="https://youtu.be/r3qNES6HLu0"
+            />
+
+
+        </div>
+    )
+}
+
+export default CeditDetails
