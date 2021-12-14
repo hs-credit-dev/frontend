1da22e9955d83fd8bc8af0e1bc72ddf1f19dd8d9
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-12-14 14:34:55 -0800

    just playing around with different carousels. jacks is still in there. i added another one. its all commented out

--- src/components/ProfileCarousel.js
+++ src/components/ProfileCarousel.js
@@ -1,102 +1,247 @@
 
-import Carousel from 'react-bootstrap/Carousel'
-import { Link } from 'react-router-dom'
+// import Carousel from 'react-bootstrap/Carousel'
+// import { Link } from 'react-router-dom'
 
-function ProfileCarousel() {
-  return (
-<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
+// function ProfileCarousel() {
+//     return (
+//         // <Carousel>
+//         //   <Carousel.Item>
+//         //     <img
+//         //       className="d-block w-100"
+//         //       src="holder.js/800x400?text=First slide&bg=373940"
+//         //       alt="First slide"
+//         //     />
+//         //     <Carousel.Caption>
+//         //       <h3>First slide label</h3>
+//         //       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         //     </Carousel.Caption>
+//         //   </Carousel.Item>
+//         //   <Carousel.Item>
+//         //     <img
+//         //       className="d-block w-100"
+//         //       src="holder.js/800x400?text=Second slide&bg=282c34"
+//         //       alt="Second slide"
+//         //     />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
+//         //     <Carousel.Caption>
+//         //       <h3>Second slide label</h3>
+//         //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         //     </Carousel.Caption>
+//         //   </Carousel.Item>
+//         //   <Carousel.Item>
+//         //     <img
+//         //       className="d-block w-100"
+//         //       src="holder.js/800x400?text=Third slide&bg=20232a"
+//         //       alt="Third slide"
+//         //     />
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+//         //     <Carousel.Caption>
+//         //       <h3>Third slide label</h3>
+//         //       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+//         //     </Carousel.Caption>
+//         //   </Carousel.Item>
+//         // </Carousel>
 
 
 
-    ////////////////////////
-    // <Carousel variant="dark">
-    //   <Carousel.Item>
-    //     <div className='item'>
-    //       <Link to='/'>
-    //             <a href="https://www.youtube.com/watch?v=r3qNES6HLu0" target='_blank'> 
-    //                 <img className='carousel'src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo"/>
-    //             </a>
-    //       </Link>
-    //     </div>
-    //     <Carousel.Caption className='caption'>
-    //       <div className='caption'>
-    //         <Link to='/'>
-    //         </Link>
-    //       </div>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
+//         //////////////////////
+//         <Carousel variant="dark">
+//             <Carousel.Item>
+//                 <div className='item'>
+//                     <Link to='/'>
 
-    //   <Carousel.Item>
-    //     <div className='item'>
-    //       <Link to='/'>
-    //             <a href="https://www.youtube.com/watch?v=r3qNES6HLu0" target='_blank'> 
-    //                 <img className='carousel'src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo"/>
-    //             </a>
-    //       </Link>
-    //     </div>
-    //     <Carousel.Caption>
-    //       <div className='caption'>
-    //         <Link to='/'>
+//                         <img className='carousel' src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo" />
 
-    //         </Link>
-    //       </div>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
+//                     </Link>
+//                 </div>
+//                 <Carousel.Caption className='caption'>
+//                     <div className='caption'>
+//                         <Link to='/'>
+//                         </Link>
+//                     </div>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
 
-    //   <Carousel.Item >
-    //     <div className='item'>
-    //       <Link to='/'>
-    //             <a href="https://www.youtube.com/watch?v=r3qNES6HLu0" target='_blank'> 
-    //                 <img className='carousel'src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo"/>
-    //             </a>
-    //       </Link>
-    //     </div>
-    //     <Carousel.Caption>
-    //       <div className='caption3'>
-    //         <Link to='/'>
-    //         </Link>
-    //       </div>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    // </Carousel>
-  )
-}
+//             <Carousel.Item>
+//                 <div className='item'>
+//                     <Link to='/'>
 
-export default ProfileCarousel
+//                         <img className='carousel' src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo" />
+
+//                     </Link>
+//                 </div>
+//                 <Carousel.Caption>
+//                     <div className='caption'>
+//                         <Link to='/'>
+
+//                         </Link>
+//                     </div>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+
+//             <Carousel.Item >
+//                 <div className='item'>
+//                     <Link to='/'>
+
+//                         <img className='carousel' src={`${process.env.PUBLIC_URL}/images/hscreditlogo.png`} alt="hs.credit logo" />
+
+//                     </Link>
+//                 </div>
+//                 <Carousel.Caption>
+//                     <div className='caption3'>
+//                         <Link to='/'>
+//                         </Link>
+//                     </div>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//         </Carousel>
+//     )
+// }
+
+// export default ProfileCarousel
+
+// import React from "react";
+// import {
+//     MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage,
+//     MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn
+// } from "mdbreact";
+
+// const ProfileCarousel = () => {
+//     return (
+//         <MDBContainer>
+//             <MDBCarousel activeItem={1} length={3} slide={true} showControls={true} showIndicators={true} multiItem>
+//                 <MDBCarouselInner>
+//                     <MDBRow>
+//                         <MDBCarouselItem itemId="1">
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                         </MDBCarouselItem>
+//                         <MDBCarouselItem itemId="2">
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(60).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(47).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(48).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                         </MDBCarouselItem>
+//                         <MDBCarouselItem itemId="3">
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(53).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                             <MDBCol md="4">
+//                                 <MDBCard className="mb-2">
+//                                     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(41).jpg" />
+//                                     <MDBCardBody>
+//                                         <MDBCardTitle>MDBCard title</MDBCardTitle>
+//                                         <MDBCardText>
+//                                             Some quick example text to build on the card title and
+//                                             make up the bulk of the card's content.
+//                                         </MDBCardText>
+//                                         <MDBBtn color="primary">MDBBtn</MDBBtn>
+//                                     </MDBCardBody>
+//                                 </MDBCard>
+//                             </MDBCol>
+//                         </MDBCarouselItem>
+//                     </MDBRow>
+//                 </MDBCarouselInner>
+//             </MDBCarousel>
+//         </MDBContainer>
+//     );
+// }
+
+// export default ProfileCarousel;
+
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+// import Carousel from 'react-bootstrap/Carousel'
 import ProfileCarousel from '../components/ProfileCarousel'
 const About = () => {
     return (
@@ -8,8 +8,8 @@ const About = () => {
 
             {/*  who we are, contact, link to other website */}
             <p>At hs.credit, we are the gold standard of academic testing. Each month 11th and 12th grade students can mint one credit-bearing NFT, approved by a committee of 3 credit experts. Socio-economically diverse blocks of these NFTs generate academic capital, a cryptocurrency award for participating students. Educators earn up to $180/hr as credit experts on the platform. The result is quality academic data and a steady stream of high quality, hyper-local youth media.</p>
-        
-        <ProfileCarousel />
+
+            <ProfileCarousel />
         </div>
     )
 }
