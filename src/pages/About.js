a2a75418a6e230de943f2a4ba124c09b1fc3b8aa
Leonidas <ldbrooks297@gmail.com>
2021-12-15 13:08:02 -0800

    capitalization erro causing build to fail

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/carousel'
 import ProfileCarousel from '../components/ProfileCarousel'
 const About = () => {
     return (
