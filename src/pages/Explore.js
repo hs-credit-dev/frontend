9bbb91c32546aa878d3221fc8c7db42986f3f1dc
mbrown <tugtuj@gmail.com>
2022-02-01 21:12:46 -0500

    Restyling, added search, newdummy content

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -6,51 +6,83 @@ import '../styles/explore.styles.css'
 const Explore = () => {
     return (
         <div className='explore-page-container'>
+            <input className='explore-searchbar' placeholder='search credits ...'></input>
             <div className='explore-credits-container'>
                 <div className='explore-credit-block'>
-                    <ReactPlayer className='media' width='65vw' height='65vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <ReactPlayer className='explore-media' width='35vw' height='55vh' url='https://youtu.be/0EgKJPrdaVI' />
                     <div className='credit-info-block'>
-                        <h2>Chilled Serenity</h2>
-                        <h3>By: <a href='#'>Angel Ramirez</a></h3>
-                        <h5>Studio: Sample Studio </h5>
-                        <p className='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <div className='explore-user-header'>
+                            <img className='nav-user-icon' src="https://raw.githubusercontent.com/mozilla/fxa/9ca5c4057cde5da1e2866cb9515e88bb18e5fb2b/packages/fxa-profile-server/lib/assets/default-profile.png" alt="default profile avatar" />
+                            <p className='credit-author'><a href='#'>ajr2378</a></p>
+                        </div>
+                        <h4 className='credit-title'>Into the New York School System</h4>
+                        <p className='credit-studio'>Studio: New York University</p>
+                        <p id='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
-            </div>
-            <hr/>
-            <div className='explore-credits-container'>
-                <div className='explore-credit-block'>
-                    <ReactPlayer className='media' width='65vw' height='65vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-                    <div className='credit-info-block'>
-                        <h2>Chilled Serenity</h2>
-                        <h3>By: <a href='#'>Angel Ramirez</a></h3>
-                        <h5>Studio: Sample Studio </h5>
-                        <p className='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
+                <div className='explore-divider'></div>
             </div>
             <div className='explore-credits-container'>
                 <div className='explore-credit-block'>
-                    <ReactPlayer className='media' width='65vw' height='65vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <ReactPlayer className='explore-media' width='35vw' height='55vh' url='https://youtu.be/RO_RRKTYzUY' />
                     <div className='credit-info-block'>
-                        <h2>Chilled Serenity</h2>
-                        <h3>By: <a href='#'>Angel Ramirez</a></h3>
-                        <h5>Studio: Sample Studio </h5>
-                        <p className='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <div className='explore-user-header'>
+                            <img className='nav-user-icon' src="https://raw.githubusercontent.com/mozilla/fxa/9ca5c4057cde5da1e2866cb9515e88bb18e5fb2b/packages/fxa-profile-server/lib/assets/default-profile.png" alt="default profile avatar" />
+                            <p className='credit-author'><a href='#'>NebulaNoodle</a></p>
+                        </div>
+                        <h4 className='credit-title'>Our Galaxy: Understanding Space</h4>
+                        <p className='credit-studio'>Studio: NASA </p>
+                        <p id='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
+                <div className='explore-divider'></div>
             </div>
             <div className='explore-credits-container'>
                 <div className='explore-credit-block'>
-                    <ReactPlayer className='media' width='65vw' height='65vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <ReactPlayer className='explore-media' width='35vw' height='55vh' url='https://youtu.be/k5obtOZe4ZQ' />
                     <div className='credit-info-block'>
-                        <h2>Chilled Serenity</h2>
-                        <h3>By: <a href='#'>Angel Ramirez</a></h3>
-                        <h5>Studio: Sample Studio </h5>
-                        <p className='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <div className='explore-user-header'>
+                            <img className='nav-user-icon' src="https://raw.githubusercontent.com/mozilla/fxa/9ca5c4057cde5da1e2866cb9515e88bb18e5fb2b/packages/fxa-profile-server/lib/assets/default-profile.png" alt="default profile avatar" />
+                            <p className='credit-author'><a href='#'>ajr2378</a></p>
+                        </div>
+                        <h4 className='credit-title'>The Evolution of Beauty in Art</h4>
+                        <p className='credit-studio'>Studio: Detroit Institute of Art </p>
+                        <p id='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
+                <div className='explore-divider'></div>
             </div>
+            <div className='explore-credits-container'>
+                <div className='explore-credit-block'>
+                    <ReactPlayer className='explore-media' width='35vw' height='55vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <div className='credit-info-block'>
+                        <div className='explore-user-header'>
+                            <img className='nav-user-icon' src="https://raw.githubusercontent.com/mozilla/fxa/9ca5c4057cde5da1e2866cb9515e88bb18e5fb2b/packages/fxa-profile-server/lib/assets/default-profile.png" alt="default profile avatar" />
+                            <p className='credit-author'><a href='#'>ajr2378</a></p>
+                        </div>
+                        <h4 className='credit-title'>A musical ode to Breonna </h4>
+                        <p className='credit-studio'>Studio: Black Lives Matter </p>
+                        <p id='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className='explore-divider'></div>
+            </div>
+            <div className='explore-credits-container'>
+                <div className='explore-credit-block'>
+                    <ReactPlayer className='explore-media' width='35vw' height='55vh' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <div className='credit-info-block'>
+                        <div className='explore-user-header'>
+                            <img className='nav-user-icon' src="https://raw.githubusercontent.com/mozilla/fxa/9ca5c4057cde5da1e2866cb9515e88bb18e5fb2b/packages/fxa-profile-server/lib/assets/default-profile.png" alt="default profile avatar" />
+                            <p className='credit-author'><a href='#'>ajr2378</a></p>
+                        </div>
+                        <h4 className='credit-title'>Chilled Serenity</h4>
+                        <p className='credit-studio'>Studio: Sample Studio </p>
+                        <p id='credit-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className='explore-divider'></div>
+            </div>
+
         </div>
     )
 }
