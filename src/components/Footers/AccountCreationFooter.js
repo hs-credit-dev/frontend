baa52d4464454b705e1785ca5fb310641dd2b407
Emma Yuan <jinyueyu@usc.edu>
2022-06-29 09:44:52 -0700

    change styling unit from px to em

--- src/components/Footers/AccountCreationFooter.js
+++ src/components/Footers/AccountCreationFooter.js
@@ -11,13 +11,11 @@ const AccountCreationFooter = () => {
             <img className="footer-hscredit-logo" src={logo} alt="HSC.Logo" />
             <p className="footer-text">Powered by hs.credit</p>
             <span className="footer-divider">|</span>
-            <a className="footer-text" href="https://hs.credit">https://www.hs.credit</a>
-            <span className="footer-divider">|</span>
             <a className="footer-text" href="/">Terms & Services</a>
             <span className="footer-divider">|</span>
-            <a className="footer-text" href="/">Data Use Policy</a>
+            <a className="footer-text" href="/">Privacy Policy</a>
             <span className="footer-divider">|</span>
-            <p className="footer-text">© 2022 hs.credit</p>
+            <p className="footer-text">© 2022 Academic Capital Foundation, Inc.</p>
 
         </div>
     )
