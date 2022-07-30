35573438867ec2aba46accfad8870276eaee5faf
crlimacastro <crlimacastro@gmail.com>
2022-07-30 01:52:01 -0400

    added home page

---
+++ src/components/Headers/HomeHeader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../../styles/home-header.styles.css'
+
+export const IndexHeader = () => {
+    return (
+        <div className='home-header'>
+
+            <img className="hsc-logo-landing" src='https://i.imgur.com/t5GkphG.png' alt='hsc logo' />
+            <h4 className="tagline">researched, revised, relevant</h4>
+
+
+        </div>
+    )
+}
+
+export default IndexHeader
