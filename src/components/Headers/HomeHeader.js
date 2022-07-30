a2563d7dc61ac20764a0fe687ca2c0e4259ff445
crlimacastro <crlimacastro@gmail.com>
2022-07-30 02:14:23 -0400

    cleared out some whitespace

--- src/components/Headers/HomeHeader.js
+++ src/components/Headers/HomeHeader.js
@@ -5,11 +5,8 @@ import '../../styles/home-header.styles.css'
 export const IndexHeader = () => {
     return (
         <div className='home-header'>
-
             <img className="hsc-logo-landing" src='https://i.imgur.com/t5GkphG.png' alt='hsc logo' />
             <h4 className="tagline">researched, revised, relevant</h4>
-
-
         </div>
     )
 }
