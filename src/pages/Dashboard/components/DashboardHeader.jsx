f2cc4c7fffc35fbb7523a9beb7a5833b2c90ac9c
Emma Yuan <jinyueyu@usc.edu>
2022-08-16 15:48:17 -0700

    generate user icon base on username initial

--- src/pages/Dashboard/components/DashboardHeader.jsx
+++ src/pages/Dashboard/components/DashboardHeader.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { Avatar, Typography } from "@material-tailwind/react";
 
@@ -6,6 +7,9 @@ import userIcon from "../../../assets/svg/user-avatar.svg";
 import logout from "../../../assets/svg/log-out-icon.svg";
 
 const DashboardHeader = () => {
+  const [username, setUsername] = useState("Nadav");
+  const initial = username[0];
+
   return (
     <>
       <div className="flex mt-8 mx-8">
@@ -16,8 +20,9 @@ const DashboardHeader = () => {
           </Typography>
         </NavLink>
         <div variant="h3" className="ml-auto">
-            <NavLink to="/">
+            <NavLink to="/" className="relative">
               <Avatar src={userIcon} alt="userIcon" className="mr-6 w-12 h-12"/>
+              <Typography className='text-white font-bold text-4xl inline-block absolute right-9 -top-2'>{initial}</Typography>
             </NavLink>
             <NavLink to="/">
               <Avatar src={logout} alt="logout" className="mr-6 w-8 h-8" />
