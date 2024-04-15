594cad559d883a038a14a09667fa796c7efb90d7
Mitra Singh <mitra.coder@gmail.com>
2024-04-15 19:59:18 -0400

    Add custom color purple

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				hsPurple: "#805DBE", // HS Credit Purple
+			},
+		},
+	},
+	plugins: [],
 };
