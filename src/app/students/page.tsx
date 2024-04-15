1ee01f0bd1d3059d5dc1a9e8099ab855a8d33596
Mitra Singh <mitra.coder@gmail.com>
2024-04-15 19:56:58 -0400

    Add navigation elements

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,6 +1,25 @@
 export default function Page() {
-    return (
-      <h1>Hello, Student page!</h1>
-    )
-  }
-  
+	return (
+		<div className="py-10 px-10">
+			<nav className="grid md:grid-cols-3 gap-4">
+				<div>
+					<h1 className="flex justify-center text-2xl font-bold md:justify-start lg:text-3xl">
+						Browse Credits
+					</h1>
+				</div>
+				<div className="flex justify-center md:ml-20">
+					<input
+						className="px-3 py-1 border text-sm border-gray-500 rounded-full focus:outline-none focus:border-2"
+						type="search"
+						placeholder="Search..."
+					/>
+				</div>
+				<div className="flex justify-center md:justify-end">
+					<button className="h-8 w-16 rounded-full bg-hsPurple text-white text-xs">
+						Back
+					</button>
+				</div>
+			</nav>
+		</div>
+	);
+}
