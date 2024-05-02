8c6f550afebbca1b37ad43a1e2b8aa548413fb88
Mitra Singh <mitra.coder@gmail.com>
2024-05-01 22:53:44 -0400

    add: feedback prop, awaiting feedback ui approval

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -15,6 +15,8 @@ export default function Page() {
 		setSearchInputValue(e.target.value);
 	};
 
+	const title = "Browse Credits";
+
 	return (
 		<>
 			<section className="mx-auto py-14 px-10 max-w-screen-lg md:max-w-screen-md lg:max-w-screen-lg">
@@ -39,7 +41,7 @@ export default function Page() {
 				</section>
 				<section className="fixed right-4 bottom-0 -mb-1 md:right-0 md:top-1/2 md:-mr-12">
 					<button
-						className="bg-black px-2 text-white text-xs pb-2 pt-1 rounded hover:bg-hsPurple md:pb-4 md:-rotate-90"
+						className="bg-black px-2 text-white text-xs pb-2 pt-1 rounded hover:bg-[#805DBE] md:pb-4 md:-rotate-90"
 						onClick={() => setShowModal(true)}
 					>
 						Leave Feedback
@@ -49,6 +51,7 @@ export default function Page() {
 			<FeedbackModal
 				isVisible={showModal}
 				onClose={() => setShowModal(false)}
+				pageTitle={title}
 			/>
 		</>
 	);
