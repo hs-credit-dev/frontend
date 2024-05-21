3efd9155ca8780f8829c54cf29195963e792dcfe
Mitra Singh <mitra.coder@gmail.com>
2024-05-21 00:04:00 -0400

    add: ui and functionality

---
+++ src/app/students/dashboard/DeleteButton.tsx
@@ -0,0 +1,22 @@
+const DeleteButton = () => {
+	return (
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={3}
+				stroke="#D40000"
+				className="w-6 h-6"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M6 18 18 6M6 6l12 12"
+				/>
+			</svg>
+		</button>
+	);
+};
+
+export default DeleteButton;
---
+++ src/app/students/dashboard/TakeActionButton.tsx
@@ -0,0 +1,11 @@
+const TakeActionButton = () => {
+	return (
+		<button
+			className={`h-[1.5rem] px-4 bg-[#805DBE] text-white text-[.75rem] font-normal hover:bg-violet-700`}
+		>
+			Take Action
+		</button>
+	);
+};
+
+export default TakeActionButton;
