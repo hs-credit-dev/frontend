c5fe347d35f0ae43041283d953746a2f2005ff6b
Mitra Singh <mitra.coder@gmail.com>
2024-05-23 09:23:24 -0400

    updated: change main styles, added wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,29 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Header from '@/app/_components/header/header';
-import Footer from '@/app/_components/footer/footer';
-import './globals.css'
-import WelcomeBanner from './students/dashboard/WelcomeBanner';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "@/app/_components/header/header";
+import Footer from "@/app/_components/footer/footer";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HS.Credit',
-}
+	title: "HS.Credit",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className='container'>
-          <Header actionType='standard'/>
-		  <WelcomeBanner />
-            <main>
-            {children}
-            </main>
-          <Footer/>
-        </div>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<div className="container">
+					<Header actionType="standard" />
+					<main>{children}</main>
+					<Footer />
+				</div>
+			</body>
+		</html>
+	);
 }
