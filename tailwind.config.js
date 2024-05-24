9472ba329169a7b319c0ab31475c3a6cdfda45b9
Mitra Singh <mitra.coder@gmail.com>
2024-05-24 00:45:23 -0400

    update: remove color property

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
-		extend: {
-			colors: {
-				hsPurple: "#805DBE", // HS Credit Purple
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
