de09bf139d621269c938db94ad67f3019b94df7c
Mitra Singh <mitra.coder@gmail.com>
2024-05-23 23:52:32 -0400

    update: component name change, move to folder route

---
+++ src/app/students/BackButton.tsx
@@ -0,0 +1,11 @@
+const Button = () => {
+	return (
+		<button
+			className={`h-[2.4375rem] w-[5.125rem] rounded-full bg-[#805DBE] text-white text-xs font-bold hover:bg-violet-700`}
+		>
+			Back
+		</button>
+	);
+};
+
+export default Button;
