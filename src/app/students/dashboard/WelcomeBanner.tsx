a90d1114d504b11b24df7e124385b82d7fe6e41b
Mitra Singh <mitra.coder@gmail.com>
2024-05-27 23:53:53 -0400

    update: convert height class to padding

--- src/app/students/dashboard/WelcomeBanner.tsx
+++ src/app/students/dashboard/WelcomeBanner.tsx
@@ -1,7 +1,7 @@
 const WelcomeBanner = () => {
 	return (
-		<section className="h-[10.9375rem] bg-[#805DBE] rounded-xl pb-14 mb-6 mx-12">
-			<h2 className="text-[2rem] font-bold text-white pt-[2.125rem] pl-[4.3125rem]">
+		<section className="py-[3rem] bg-[#805DBE] rounded-xl mb-6 mx-12">
+			<h2 className="text-[2rem] font-bold text-white pl-[4.3125rem]">
 				Welcome back, Nyah
 			</h2>
 			<p className="text-[0.9375rem] font-light text-white pt-[1.5rem] pl-[4.3125rem]">
