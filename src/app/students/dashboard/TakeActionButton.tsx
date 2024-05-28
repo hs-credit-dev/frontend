02471849de658b54836d3bcd3f7113dbd027c556
Mitra Singh <mitra.coder@gmail.com>
2024-05-27 23:39:00 -0400

    update: convert height to padding class

--- src/app/students/dashboard/TakeActionButton.tsx
+++ src/app/students/dashboard/TakeActionButton.tsx
@@ -1,7 +1,7 @@
 const TakeActionButton = () => {
 	return (
 		<button
-			className={`h-[1.5rem] px-4 bg-[#805DBE] text-white text-[.75rem] font-normal hover:bg-violet-700`}
+			className={`py-[1 rem] px-4 bg-[#805DBE] text-white text-[.75rem] font-normal hover:bg-violet-700`}
 		>
 			Take Action
 		</button>
