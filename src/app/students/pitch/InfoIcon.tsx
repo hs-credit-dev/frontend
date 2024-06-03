797186a22fc2faba985cea6e743eef41d3e53e64
Mitra Singh <mitra.coder@gmail.com>
2024-06-03 17:07:58 -0400

    add: global info icon component

---
+++ src/app/students/pitch/InfoIcon.tsx
@@ -0,0 +1,21 @@
+export type InfoIcon = {
+	message: string;
+};
+
+const InfoIcon = ({ message }: InfoIcon) => {
+	return (
+		<button className="relative flex items-center group">
+			<figure className="ml-2 w-5 cursor-default rounded-full bg-black text-center text-white text-sm">
+				?
+			</figure>
+			<div className="absolute left-0 items-center hidden ml-6 group-hover:flex">
+				<div className="mr-4"></div>
+				<span className="relative z-10 p-2 w-[12rem] text-xs leading-none text-white whitespace-no-wrap bg-black rounded-md">
+					{message}
+				</span>
+			</div>
+		</button>
+	);
+};
+
+export default InfoIcon;
