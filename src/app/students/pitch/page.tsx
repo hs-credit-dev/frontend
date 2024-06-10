d79660d253d2e5d8c9c76a3aa0f6a0786b6a6300
Mitra Singh <mitra.coder@gmail.com>
2024-06-09 21:32:54 -0400

    update: move form to own component, other minor changes

--- src/app/students/pitch/page.tsx
+++ src/app/students/pitch/page.tsx
@@ -1,5 +1,6 @@
 import BackButton from "./BackButton";
 import InfoIcon from "./InfoIcon";
+import PitchUploadForm from "./PitchUploadForm";
 
 export default function PitchPage() {
 	return (
@@ -9,82 +10,29 @@ export default function PitchPage() {
 				<BackButton />
 			</div>
 
-			<section className="pr-[5rem]">
-				<p className="text-sm py-[4rem]">
-					You invested your attention to STAKE this credit. Next you upload a
-					plan for 110+ hours of research, revision, reflection to produce your
-					10-minutes of polished audio or video. Remember to let the rubric
-					guide your planning as it will determine your success as you MINT the
-					final product.
-				</p>
-				<form>
-					<div className="flex flex-col gap-2">
-						<div className="inline-flex mb-[.4rem]">
-							<label>Credit in Progress</label>
-							<InfoIcon message="This the current credit you are working on." />
-						</div>
-						<textarea
-							className="bg-[#E0E0E0] py-[1.25rem] w-[28.125rem] text-center justify-center rounded-md shadow-md resize-none focus:outline-none"
-							readOnly
-							rows={1}
-						>
-							BrandeisMedia: Podcast episode about foster care
-						</textarea>
+			<p className="text-sm py-[4rem] pr-10">
+				You invested your attention to STAKE this credit. Next you upload a plan
+				for 110+ hours of research, revision, reflection to produce your
+				10-minutes of polished audio or video. Remember to let the rubric guide
+				your planning as it will determine your success as you MINT the final
+				product.
+			</p>
 
-						<div className="inline-flex mt-[2.5rem] mb-[.4rem]">
-							<label>Task</label>
-							<InfoIcon message="No sentence provided." />
-						</div>
-						<button
-							className="h-[2.4375rem] 
-                  w-[10.125rem] 
-                  rounded-full 
-                  bg-[#805DBE] 
-                  text-white 
-                  text-xs 
-                  font-bold 
-                  hover:bg-violet-700"
-						>
-							Click here to upload
-						</button>
-						<p className="text-xs">
-							Please make sure your attachment doesn’t exceed 25 MB.
-						</p>
-
-						<div className="inline-flex mt-[2.5rem] mb-[.4rem]">
-							<label className="text-sm">
-								Checkpoints <span className="text-sm">(Optional)</span>
-							</label>
-							<InfoIcon message="Add/Edit key milestones to keep you and your teacher on the same page." />
-						</div>
-						<button
-							className="h-[2.4375rem] 
-                  w-[10.125rem] 
-                  rounded-full
-                  border-[#805DBE]
-                  border-2
-                  text-black 
-                  text-xs 
-                  font-bold"
-						>
-							Add/Edit
-						</button>
-
-						<button
-							className="mt-[2rem] h-[2.4375rem] 
-                  w-[10.125rem] 
-                  rounded-full 
-                  bg-[#805DBE] 
-                  text-white 
-                  text-xs 
-                  font-bold 
-                  hover:bg-violet-700"
-						>
-							Submit
-						</button>
-					</div>
-				</form>
-			</section>
+			<div className="gap-2">
+				<div className="flex flex-row mb-[.4rem]">
+					<label>Credit in Progress</label>
+					<InfoIcon message="This the current credit you are working on." />
+				</div>
+				<textarea
+					className="bg-[#E0E0E0] py-4 px-4 text-center justify-center rounded-md shadow-md resize-none focus:outline-none"
+					readOnly
+					rows={1}
+					cols={45}
+				>
+					BrandeisMedia: Podcast episode about foster care
+				</textarea>
+			</div>
+			<PitchUploadForm />
 		</section>
 	);
 }
