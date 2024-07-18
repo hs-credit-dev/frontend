969bdacb347677c7cfeae310e6b28b769f5ce62c
mitrasingh <mitra.coder@gmail.com>
2024-07-18 16:41:20 -0400

    update: git status git status '

--- src/app/partners/dashboard/page.tsx
+++ src/app/partners/dashboard/page.tsx
@@ -5,8 +5,6 @@ import { useState } from "react";
 import OwnerCreditList from "./OwnerCreditList";
 
 export default function Page() {
-	const [showModal, setShowModal] = useState<boolean>(false);
-
 	const title = "Owner Credits";
 
 	return (
@@ -22,20 +20,7 @@ export default function Page() {
 				<section className="mx-0 mt-10 max-h-full overflow-y-scroll grid justify-items-center md:pr-6 md:mt-10 md:grid-cols-4 gap-6 md:justify-items-start">
 					<OwnerCreditList />
 				</section>
-				<section className="fixed right-4 bottom-0 -mb-1 md:right-0 md:top-1/2 md:-mr-12">
-					<button
-						className="bg-black px-2 text-white text-xs pb-2 pt-1 rounded hover:bg-[#805DBE] md:pb-4 md:-rotate-90"
-						onClick={() => setShowModal(true)}
-					>
-						Leave Feedback
-					</button>
-				</section>
 			</section>
-			<FeedbackModal
-				isVisible={showModal}
-				onClose={() => setShowModal(false)}
-				pageTitle={title}
-			/>
 		</>
 	);
 }
