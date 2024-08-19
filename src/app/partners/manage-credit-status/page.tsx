d9e82434de0436092f4fef0cae917a3205b8bc3c
mitrasingh <mitra.coder@gmail.com>
2024-08-18 20:42:51 -0400

    update: add additional components

--- src/app/partners/manage-credit-status/page.tsx
+++ src/app/partners/manage-credit-status/page.tsx
@@ -1,5 +1,6 @@
 import BackButton from "./BackButton";
 import DropDown from "./DropDown";
+import FileUpload from "./FileUpload";
 
 export default function ManageCreditStatus() {
 	return (
@@ -10,6 +11,21 @@ export default function ManageCreditStatus() {
 			</div>
 			<p className="text-md mt-6 mb-2 text-gray-800">Discipline</p>
 			<DropDown />
+			<button className="btn bg-green-600 text-[0.75rem] px-[3.6875rem] py-3">
+				Publish
+			</button>
+			<div>
+				<p className="text-md mt-6 mb-2 text-gray-800">Credit Name</p>
+				<input className="z-0 resize-none w-full runded drop-shadow-sm hover:drop-shadow focus:outline-none" />
+			</div>
+			<FileUpload />
+			<div>
+				<p className="text-md mt-6 mb-2 text-gray-800">Add Admins</p>
+				<input
+					value="Elizabeth Simmons - elizabeth@hs.credit"
+					className="z-0 resize-none w-full runded drop-shadow-sm hover:drop-shadow focus:outline-none"
+				/>
+			</div>
 		</section>
 	);
 }
