e58648046c5cab2c3fe7f918ff5f8a0b56bf4491
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2024-09-25 13:23:23 +0200

    User can see appropriate header on certain pages and banner

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import Typography from '../../components/Typography';
 
 const Header = () => {
+	const router = useRouter();
+
+	const isAccountCreationPage = router.pathname === '/signup';
+
 	return (
 		<div className='container mx-auto pt-10 pb-5'>
 			<div className='flex flex-col sm:flex-row justify-between items-center w-full'>
-				<Typography className='text-black text-base sm:pl-8'>hs.credit</Typography>
-				<Typography className='text-black text-3xl font-bold text-center sm:text-right'>
-					Account Creation
-				</Typography>
+				{isAccountCreationPage ? (
+					<>
+						<Typography className='text-black text-base sm:pl-8'>hs.credit</Typography>
+						<Typography className='text-black text-3xl font-bold text-center sm:text-right'>
+							Account Creation
+						</Typography>
+					</>
+				) : (
+					<div className='flex justify-between items-center w-full'>
+						<div className='flex items-center'>
+							<Image
+								src='/images/icons/hscreditlogonowords3.png'
+								alt='logo'
+								width={70}
+								height={70}
+							/>
+							<Typography className='ml-2 text-[22px] font-bold'>hs.credit</Typography>
+						</div>
+						<div className='flex items-center space-x-4'>
+							<Image src='/images/icons/nIcon.png' alt='logo' width={40} height={40} />
+							<Image src='/images/icons/quitIcon.png' alt='logo' width={27} height={27} />
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
