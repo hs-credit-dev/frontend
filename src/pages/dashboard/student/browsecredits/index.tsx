aaa99f9a1badb42e9e6dfc092e528e1f7617f51c
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2024-09-28 00:33:36 +0200

    Fixed imports

--- src/pages/dashboard/student/browsecredits/index.tsx
+++ src/pages/dashboard/student/browsecredits/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Button from '../../../components/Button';
-import Typography from '../../../components/Typography';
-import Page from '../../../layout/Page';
+import Button from '../../../../components/Button';
+import Typography from '../../../../components/Typography';
+import Page from '../../../../layout/Page';
 
 import Card from './Card';
 
