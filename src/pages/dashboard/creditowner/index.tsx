b9480eda8fa6c6a812f00db489364ea752c9fa1f
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2024-09-28 23:15:06 +0200

    Created credit owner page

--- src/pages/dashboard/creditowner/index.tsx
+++ src/pages/dashboard/creditowner/index.tsx
@@ -1,5 +1,107 @@
+import React from 'react';
+
+import Typography from '../../../components/Typography';
+import Page from '../../../layout/Page';
+import Card from '../student/browsecredits/Card';
+
+type CardData = {
+	imageSrc: string;
+	imageAlt: string;
+	title: string;
+};
+
+const cardData: CardData[] = [
+	{
+		imageSrc: '/images/filmmaking.png',
+		imageAlt: 'filmmaking',
+		title: 'Filmmaking',
+	},
+	{
+		imageSrc: '/images/africanAmericanStudies.png',
+		imageAlt: 'African american studies',
+		title: 'African American Studies',
+	},
+	{
+		imageSrc: '/images/cognitiveScience.png',
+		imageAlt: 'cognitive science',
+		title: 'Cognitive Science',
+	},
+	{
+		imageSrc: '/images/musicTheory.png',
+		imageAlt: 'music theory',
+		title: 'Music Theory',
+	},
+	{
+		imageSrc: '/images/dataScience.png',
+		imageAlt: 'data science',
+		title: 'Data Science',
+	},
+	{
+		imageSrc: '/images/digitalAnimation.png',
+		imageAlt: 'digital animation',
+		title: 'Digital Animation',
+	},
+	{
+		imageSrc: '/images/businessAdministration.png',
+		imageAlt: 'business administration',
+		title: 'Business Administration',
+	},
+	{
+		imageSrc: '/images/clinicalPsychology.png',
+		imageAlt: 'clinical psychology',
+		title: 'Clinical Psychology',
+	},
+	{
+		imageSrc: '/images/clinicalPsychology.png',
+		imageAlt: 'clinical psychology',
+		title: 'Clinical Psychology',
+	},
+	{
+		imageSrc: '/images/digitalAnimation.png',
+		imageAlt: 'digital animation',
+		title: 'Digital Animation',
+	},
+	{
+		imageSrc: '/images/businessAdministration.png',
+		imageAlt: 'business administration',
+		title: 'Business Administration',
+	},
+	{
+		imageSrc: '/images/clinicalPsychology.png',
+		imageAlt: 'clinical psychology',
+		title: 'Clinical Psychology',
+	},
+	{
+		imageSrc: '/images/clinicalPsychology.png',
+		imageAlt: 'clinical psychology',
+		title: 'Clinical Psychology',
+	},
+];
+
 const CreditOwner = () => {
-	return <div>CreditOwner</div>;
+	return (
+		<Page>
+			<div className='bg-white rounded-[20px] flex flex-col box-border p-14'>
+				<div className='flex justify-between mb-20'>
+					<Typography className='font-montserrat text-[32px] font-bold leading-[39.01px] text-left'>
+						Credits
+					</Typography>
+				</div>
+				<div className='overflow-y-auto max-h-[calc(100vh-130px-140px-120px-56px)] pr-4 custom-scrollbar'>
+					<div className='flex flex-wrap gap-2 md:gap-4 lg:gap-10'>
+						{cardData.map((card, index) => (
+							<Card
+								key={index}
+								imageSrc={card.imageSrc}
+								imageAlt={card.imageAlt}
+								title={card.title}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		</Page>
+	);
 };
 
 export default CreditOwner;
